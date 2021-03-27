ea82ea4e54764af24372144d53ef75fd2818e6af
Maria Veronica Miyazono <velokita@hotmail.com>
2021-03-27 13:11:18 -0300

    entrega

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    { 
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    { 
+        title: 'Catálogo',
+        url: '#',
+        cName: 'nav-links'
+    },
+    { 
+        title: 'Contactanos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    { 
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
