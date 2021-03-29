93fccde597e146afc88a970556e3ad46aa1583d9
Maria Veronica Miyazono <velokita@hotmail.com>
2021-03-28 22:59:56 -0300

    28

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,36 +1,46 @@
-import React , {Component} from 'react';
-import {Button} from './Button';
-import {MenuItems} from "./MenuItems"
-import './NavBar.css'
+import React, { Component } from "react";
+import { Button } from "./Button";
+import { MenuItems } from "./MenuItems";
+import "./NavBar.css";
 
 class NavBar extends Component {
-    state={clicked: false}
+  state = {
+    clicked: false,
+  };
 
-    handleClick=() => {
-        this.setState ({clicked: !this.state.clicked})
-    }
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  };
 
-    render(){
-        return (
-        <nav className="NavBarItems">
-            <h1 className="navbar-logo">FiKa React<i className="fab fa-react"></i> </h1>
-            <div className="menu-icon" onClick={this.handleClick}>                 
-                <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}> </i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item,index)=> {
-                        return (
-                            <li key={index}>
-                                <a className={item.cName} href={item.url} > {item.title} </a>
-                            </li>                            
-                        )
-                    })}
-                </ul>
-                <Button>Mi Carrito</Button>
-        </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="NavBarItems">
+        <h1 className="navbar-logo">
+          FiKa React <i className="fab fa-react"> </i>
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}>
+           </i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button> Mi Carrito </Button>
+      </nav>
+    );
+  }
 }
 
-
-export default NavBar
+export default NavBar;
