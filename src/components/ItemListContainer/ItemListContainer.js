fc76221c19d9595dde447aa9da9f88ae7cf5d8d9
Maria Veronica Miyazono <velokita@hotmail.com>
2021-04-03 11:26:12 -0300

    Desafio Estilos y Home

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -6,11 +6,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
+import "./ItemListContainer.css";
+
 function ItemListContainer({mensaje}) {
     return (    
         <Jumbotron fluid>
             <Container>
-            <h2>{mensaje}</h2>
+            <h2 className="titulo">{mensaje}</h2>
             <Row>
                 <Col xs={6} md={4}>
                         <Image src={imagen} rounded />
--- src/components/NavBar/MenuItems.js
+++ src/components/NavBar/MenuItems.js
@@ -1,6 +1,6 @@
 export const MenuItems = [
     { 
-        title: 'Inicio',
+        title: 'Nuestra Historia',
         url: '#',
         cName: 'nav-links'
     },
