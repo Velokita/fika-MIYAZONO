83e2faba12fcfad117951a42afa883d9506e7a78
Maria Veronica Miyazono <veromiya@gmail.com>
2021-04-29 23:39:22 -0300

    desafio context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -13,7 +13,7 @@ export const CartProvider = (props) => {
 
   const clearCart = () => {
     // Remover todos los items
-    setCartItems = [];
+    setCartItems([]);
   };
 
   const removeItem = (itemId) => {
